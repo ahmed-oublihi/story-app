07f1bbd26cd575aaa39dbca2b6335973d598a2a2
Ahmed <oublihi.a@gmail.com>
2021-03-31 16:11:16 +0100

    Delete Post

--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -1,7 +1,11 @@
+/* eslint-disable import/no-anonymous-default-export */
 export default (posts = [], action) => {
   switch (action.type) {
     case 'UPDATE':
       return posts.map((post) => post._id === action.payload._id ? action.payload : post);
+
+    case 'DELETE':
+      return posts.filter((post) => post._id !== action.payload)
 
     case 'FETCH_ALL':
       return action.payload; 
