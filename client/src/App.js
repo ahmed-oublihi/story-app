3c686ce78b4fd378fcbde9192149efbd1d7189ea
Ahmed <oublihi.a@gmail.com>
2021-04-02 15:01:07 +0100

    last small touches

--- client/src/App.js
+++ client/src/App.js
@@ -29,7 +29,7 @@ const App = () => {
       </AppBar>
       <Grow in>
         <Container>
-          <Grid container justify='space-between' alignItems='stretch'>
+          <Grid className={classes.mainContainer} container justify='space-between' alignItems='stretch'>
             <Grid item xs={12} sm={7}>
               <Posts setCurrentId={setCurrentId} />
             </Grid>
--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -33,12 +33,12 @@ const Post = ({ post, setCurrentId }) => {
       </div>
       <Typography variant='h5' gutterBottom>{post.title}</Typography>
       <CardContent>
-        <Typography className={classes.title} variant='h5' gutterBottom>{post.message}</Typography>
+        <Typography className={classes.title} variant='body2' color='textSecondary' component='p' >{post.message}</Typography>
       </CardContent>
       <CardActions className={classes.cardActions}>
         <Button size='small' color='primary' onClick={() => dispatch(likePost(post._id))}>
           <ThumbUpAltIcon fontSize='small' />
-          Like {` `}
+          &nbsp; Like &nbsp;
           ({post.likeCount})
         </Button>
         <Button size='small' color='primary' onClick={() => dispatch(deletePost(post._id))}>
